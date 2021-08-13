9ec43b8d1d9f0ff154a1aa0e6443844f19a6f9b7
vexpands <vexpands@gmail.com>
2021-08-13 17:41:10 +0530

    about page added

--- next-video-conf/pages/about/index.js
+++ next-video-conf/pages/about/index.js
@@ -6,11 +6,11 @@ function Index(props) {
     return (
         <Fragment>
             <section className="pt-4 pt-md-11">
-                <div className="container">
+                <div className="container container_wdth">
                     <div className="row align-items-center">
                         <div className="col-12 col-md-7 col-lg-6 order-md-1 aos-init aos-animate" data-aos="fade-up">
 
-                            <h1 className="display-3 text-center text-md-start">
+                            <h1 className="display-2 text-center text-md-start">
                                 Small team. Big hearts.
                             </h1>
 
@@ -19,8 +19,54 @@ function Index(props) {
                             </p>
                       
                         </div>
-
                     </div>
+
+                    <div class="row gx-4">
+                      <div class="col-4 pad_10">
+
+                        <img src={'/images/photos/photo-16.jpg'}
+                                     className="img-fluid rounded img_shadow"
+                                     alt="..." />
+
+                      </div>
+                      <div class="col-3 pad_10">
+
+                        <img src={'/images/photos/photo-17.jpg'}
+                                     className="img-fluid rounded img_shadow mb-4"
+                                     alt="..." />
+
+                        <img src={'/images/photos/photo-18.jpg'}
+                                     className="img-fluid rounded img_shadow"
+                                     alt="..." />
+
+                      </div>
+                      <div class="col-5 pad_10">
+
+                        <div class="row gx-5 mb-4">
+                          <div class="col-5 pad_lft_ryt">
+
+                            <img src={'/images/photos/photo-19.jpg'}
+                                     className="img-fluid rounded img_shadow"
+                                     alt="..." />
+
+                          </div>
+                          <div class="col-7 pad_lft_ryt">
+
+                            <img src={'/images/photos/photo-20.jpg'}
+                                     className="img-fluid rounded img_shadow"
+                                     alt="..." />
+
+
+                          </div>
+                        </div>
+
+                        <img src={'/images/photos/photo-21.jpg'}
+                                     className="img-fluid rounded img_shadow"
+                                     alt="..." />
+
+
+                      </div>
+                  </div>
                 </div>
             </section>
         </Fragment>
